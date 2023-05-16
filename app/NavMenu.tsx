ca67743324c2397188fcd0cc15f00c60a6a4e8b4
vincentYoung <vinni.xander@gmail.com>
2023-05-16 15:37:15 -0600

    add navigation menu

---
+++ app/NavMenu.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import styles from "./NavMenu.module.css";
+import Image from "next/image";
+
+export default function NavMenu() {
+  return (
+    <nav className={styles.nav}>
+      <Link href='/'>
+        <Image
+          src='/logo.svg'
+          alt='NextSpace Logo'
+          width={216}
+          height={30}
+        />
+      </Link>
+      <ul className={styles.links}>
+        <li>
+          <Link href={"/about"}>About</Link>
+        </li>
+        <li>
+          <Link href={"/blog"}>Blog</Link>
+        </li>
+        <li>
+          <Link href={"/users"}>Users</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
