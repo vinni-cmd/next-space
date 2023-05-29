862410bae6f643918400828f28869ffca531bb4f
vincentYoung <vinni.xander@gmail.com>
2023-05-29 12:32:21 -0600

    build Users page

---
+++ app/users/page.tsx
@@ -0,0 +1,21 @@
+import UserCard from "@/components/UserCard/UserCard";
+import styles from "./page.module.css";
+import { prisma } from "@/lib/prisma";
+
+export default async function Users() {
+  const users = await prisma.user.findMany();
+  console.log("userssss::  ", users);
+
+  return (
+    <div className={styles.grid}>
+      {users.map((user) => {
+        return (
+          <UserCard
+            key={user.id}
+            {...user}
+          />
+        );
+      })}
+    </div>
+  );
+}
