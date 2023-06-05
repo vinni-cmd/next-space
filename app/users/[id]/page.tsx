03afd15e78adb445eaef69d22e548ed24a3925b2
vincentYoung <vinni.xander@gmail.com>
2023-06-05 07:56:11 -0600

    implement follow UI

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,3 +1,4 @@
+import FollowButton from "@/components/FollowButton/FollowButton";
 import { prisma } from "@/lib/prisma";
 import { Metadata } from "next";
 
@@ -28,6 +29,8 @@ export default async function UserProfile({ params }: Props) {
 
       <h3>Bio</h3>
       <p>{bio}</p>
+      {/* @ts-expect-error Server Component */}
+      <FollowButton targetUserId={params.id} />
     </div>
   );
 }
